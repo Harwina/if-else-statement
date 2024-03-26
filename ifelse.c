d7b0543215300d39dadce9403c7c1db69c76e0a2
Harwina <164463440+Harwina@users.noreply.github.com>
2024-03-26 21:48:05 +0530

    Add files via upload

---
+++ ifelse.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+main()
+{
+	int a,b;
+	printf("enter the value of A=");
+	scanf("%d",&a);
+	printf("enter the value of B=");
+	scanf("%d",&b);
+	
+	if(a<b)
+	{
+		printf("the minimum value is");
+	}
+//	else if(a>b){
+//		printf("the maximum value is");
+//	}
+	else {
+		printf(" a and b are equal");
+	}
+	
+	
+}
